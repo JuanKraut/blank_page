4ee844d9a3a8d1d1cdd680b43de172da1d1f0da8
Juan Carlos Garassino <jcgarassino@profile.local>
2018-03-08 18:08:28 +0100

    Minimal scaffolding for coding around. Basic setup

---
+++ README.md
@@ -0,0 +1,48 @@
+## Git
+```
+ git clone
+```
+## npm
+```
+ npm init
+ ```
+## Webpack
+ ```
+ npm install --save-dev webpack webpack-dev-server webpack-cli
+ ```
+Then `touch webpack.config.js` and change entry point "main" @package.json
+
+ ## CSS
+ ```
+ npm install --save-dev style-loader css-loader
+ ```
+
+ ## SASS
+ ```
+ npm install --save-dev sass-loader node-sass
+ ```
+ ## Plugins
+ ```
+ npm install html-webpack-plugin extract-text-webpack-plugin --save-dev
+ ```
+## Babel babel-plugin-transform-regenerator
+```
+npm install --save-dev
+            babel-core
+            babel-cli 
+            babel-loader 
+            babel-preset-env*
+            babel-preset-es2015* 
+            babel-polyfill
+            babel-plugin-transform-regenerator**
+```
+`touch .babelrc` and include *{ "presets": [*], "plugins": [**] }* @package.json, and also as entry @webpack.config.js
+
+## React
+```
+npm install --save-dev babel-preset-react
+```
+
+---
+
+
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  /* INPUT
+  * Is filename or a set of named filenames which act as the entry 
+  * point to build your project. You can pass multiple entries 
+  * (every entry is loaded on startup). If you pass a pair in the 
+  * form <name>=<request> you can create an additional entry point. 
+  * It will be mapped to the configuration option entry.
+  */
+  entry: ['babel-polyfill','./src/index.js'],
+  /* OUTPUT
+  * A path and filename for the bundled file to be saved in. 
+  * It will be mapped to the configuration options output.path 
+  * and output.filename
+  */
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  /* WEBPACK-DEV-SERVER
+  * https://webpack.js.org/configuration/dev-server/#devserver-contentbase
+  * https://webpack.js.org/configuration/dev-server/#devserver-publicpath-
+  * https://webpack.js.org/configuration/dev-server/#devserver-port
+  */
+  devServer: {
+    contentBase: './dist/'
+  },
+  /* PLUGINS
+  * https://webpack.js.org/plugins/html-webpack-plugin/
+  */
+  plugins: [
+    new HtmlWebpackPlugin({ title: 'blank_page', template: './src/index.html'}),
+    new ExtractTextPlugin({ filename: 'app.bundle.css' })
+  ],
+  /*
+  * 
+  */
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  }
+  /*
+  *
+  */
+};
